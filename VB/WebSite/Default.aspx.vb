fa5bdd2e4ed5a6d58b47c53002c057caf007639d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,39 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		If (Not hf.Contains("state")) OrElse hf("state").ToString() <> "edit" Then
+			Return
+		End If
+
+		Dim template As New ReadOnlyTemplate()
+
+        TryCast(grid.Columns("UnitPrice"), GridViewDataColumn).EditItemTemplate = template
+        TryCast(grid.Columns("UnitsInStock"), GridViewDataColumn).EditItemTemplate = template
+        TryCast(grid.Columns("UnitsOnOrder"), GridViewDataColumn).EditItemTemplate = template
+	End Sub
+End Class
+
+Friend Class ReadOnlyTemplate
+	Implements ITemplate
+	Public Sub InstantiateIn(ByVal _container As Control) Implements ITemplate.InstantiateIn
+		Dim container As GridViewEditItemTemplateContainer = TryCast(_container, GridViewEditItemTemplateContainer)
+
+		Dim lbl As New ASPxLabel()
+		lbl.ID = "lbl"
+
+		container.Controls.Add(lbl)
+		lbl.Text = container.Text
+	End Sub
+End Class
